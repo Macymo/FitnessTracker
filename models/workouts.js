15572e89a40386eee434739f1fad3e255eef498d
Macy <59153195+Macymo@users.noreply.github.com>
2020-07-09 23:03:38 -0400

    added models/routes

---
+++ models/workouts.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutsSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for a workout"
+  },
+//   value: {
+//     type: Number,
+//     required: "Enter an amount"
+//   }
+ 
+});
+
+const Workout = mongoose.model("Workout", workoutsSchema);
+
+module.exports = Workout;
