4c67d5e21b130bcc83f649f238f7b142109623e5
Macy <59153195+Macymo@users.noreply.github.com>
2020-07-10 21:21:18 -0400

    fixed apiRoutes, created Schema

--- models/workouts.js
+++ models/workouts.js
@@ -3,17 +3,45 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutsSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for a workout"
-  },
-//   value: {
-//     type: Number,
-//     required: "Enter an amount"
-//   }
- 
+    day: {
+        type: Date, default: new Date
+    },
+    exercises: [{
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter a name for a workout"
+        },
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter a type of workout"
+        },
+        duration: {
+            type: Number,
+            trim: true,
+            required: "Enter your duration for this exercise"
+        },
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number
+        
+}]
+    
+},
+    {
+    toJSON:{
+        virtuals: true
+    }
 });
+
+workoutsSchema.virtual("totalDuration").get(function(){
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    },0)
+})
+
 
 const Workout = mongoose.model("Workout", workoutsSchema);
 
